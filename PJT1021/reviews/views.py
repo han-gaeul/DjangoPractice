2045f8554c6a2e57a055e03bd3abb2a3001ce54a
han-gaeul <gmlakdcjs314@gmail.com>
2022-10-23 00:12:26 +0900

    reviews - index

--- PJT1021/reviews/views.py
+++ PJT1021/reviews/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Review, Comment
+from .forms import CommentForm, ReviewForm
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 # Create your views here.
+# 리뷰 목록
+def index(request):
+    reviews = Review.objects.order_by('-pk')
+    context = {
+        'reviews' : reviews
+    }
+    return render(request, 'reviews/index.html', context)
