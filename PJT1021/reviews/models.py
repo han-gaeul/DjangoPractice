0e895123e514113c6ddcb8dd5921ae9dfecc23c6
han-gaeul <gmlakdcjs314@gmail.com>
2022-10-23 00:02:34 +0900

    reviews - models.py

--- PJT1021/reviews/models.py
+++ PJT1021/reviews/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.conf import settings
+from imagekit.models import ProcessedImageField, ImageSpecField
+from imagekit.processors import ResizeToFill, Thumbnail
 
 # Create your models here.
+class Review(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    movie_name = models.CharField(max_length=50)
+    grade = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    image = ProcessedImageField(
+        upload_to = 'images/',
+        blank = True,
+        processors=[ResizeToFill(500, 500)],
+        format='JPEG',
+        options={'quality' : 100},
+    )
+    Thumbnail = ImageSpecField(
+        source='image',
+        processors=[Thumbnail(300, 300)],
+        format='JPEG',
+        options={'quality' : 100},
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=''
+    )
+
+class Comment(models.Model):
+    review = models.ForeignKey(Review, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField(max_length=1000)
