2fba900e5c0b6daf8a508dfbe90448a8bf46cfbc
han-gaeul <gmlakdcjs314@gmail.com>
2022-10-23 23:09:49 +0900

    accounts - update

--- PJT1021/accounts/views.py
+++ PJT1021/accounts/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
 from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -68,3 +69,18 @@ def detail(request, pk):
         'user' : user
     }
     return render(request, 'accounts:detail.html', context)
+
+# 회원 정보 수정
+@login_required
+def update(request):
+    if request.method == 'POST':
+        form = CustomUserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('reviews:index')
+    else:
+        form = CustomUserChangeForm(instance=request.user)
+    context = {
+        'form' : form
+    }
+    return render(request, 'accounts/update.html', context)
