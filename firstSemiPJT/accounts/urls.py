8acc16ee238e807e02db938c4dbe80d1e13c6bc0
han-gaeul <gmlakdcjs314@gmail.com>
2022-11-07 01:49:49 +0900

    modify accounts urls

--- firstSemiPJT/accounts/urls.py
+++ firstSemiPJT/accounts/urls.py
@@ -5,4 +5,10 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('password/', views.change_password, name='change_password'),
+    path('profile/', views.profile, name='profile'),
+    path('profile/update/', views.profile_update, name='profile_update'),
+    path('<int:pk>/follow/', views.follow, name='follow'),
 ]
