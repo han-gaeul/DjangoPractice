1f90c66193b7e0e9a740c0285ba3dc7633f3ed73
han-gaeul <gmlakdcjs314@gmail.com>
2022-11-11 21:14:24 +0900

    modify pjt settings

--- secondSemiPJT/secondSemiPJT/urls.py
+++ secondSemiPJT/secondSemiPJT/urls.py
@@ -1,4 +1,4 @@
-"""secondSemiPJT URL Configuration
+"""pjt URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from accounts import views
 
 urlpatterns = [
+    path('',views.index),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('snacks/', include('snacks.urls')),
