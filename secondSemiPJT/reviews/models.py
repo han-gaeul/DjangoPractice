2fe49baff96c957c013d365031e35e770d5b9101
han-gaeul <gmlakdcjs314@gmail.com>
2022-11-13 23:09:42 +0900

    리뷰/댓글 작성시간 표시 수정

--- secondSemiPJT/reviews/models.py
+++ secondSemiPJT/reviews/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from imagekit.models import ProcessedImageField, ImageSpecField
 from imagekit.processors import ResizeToFill, Thumbnail
 from accounts.models import User
+from datetime import datetime, timedelta
+from  django.utils import timezone
 
 # Create your models here.
 class Review(models.Model):
@@ -20,12 +22,42 @@ class Review(models.Model):
         options={'quality' : 100},
     )
     # stars = models.IntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='등록 시간')
     updated_at = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='review_user')
+    @property
+    def created_string(self):
+        time = datetime.now(tz=timezone.utc) - self.created_at
+
+        if time < timedelta(minutes=1):
+            return '방금 전'
+        elif time < timedelta(hours=1):
+            return str(int(time.seconds / 60)) + '분 전'
+        elif time < timedelta(days=1):
+            return str(int(time.seconds / 3600)) + '시간 전'
+        elif time < timedelta(days=2):
+            time = datetime.now(tz=timezone.utc).date() - self.created_at.date()
+            return str(time.days) + '일 전'
+        else:
+            return False
 
 class Comment(models.Model):
     content = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comment_user')
     review = models.ForeignKey(Review, on_delete=models.CASCADE, related_name='comments')
+    @property
+    def created_string(self):
+        time = datetime.now(tz=timezone.utc) - self.created_at
+
+        if time < timedelta(minutes=1):
+            return '방금 전'
+        # elif time < timedelta(hours=1):
+        #     return str(int(time.seconds / 60)) + '분 전'
+        # elif time < timedelta(days=1):
+        #     return str(int(time.seconds / 3600)) + '시간 전'
+        # elif time < timedelta(days=2):
+        #     time = datetime.now(tz=timezone.utc).date() - self.created_at.date()
+        #     return str(time.days) + '일 전'
+        else:
+            return False
